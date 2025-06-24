687b49ff2e3afe8c2013d704b57ddede12888522
anamika0707 <141743181+anamika0707@users.noreply.github.com>
2025-06-24 19:16:31 +0000

    async await

---
+++ restart/async.js
@@ -0,0 +1,15 @@
+//async function getData(){
+//setTimeout(function(){
+//console.log("i am inside timeout block")},3000)
+//}
+
+//let ans=getData();
+
+
+//await
+
+//fetch API
+
+async function getData(){
+    let response=await fetch()
+}
