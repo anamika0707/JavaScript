c4892a89c2a3bf9741337ce907a5ed3cea92a963
anamika0707 <141743181+anamika0707@users.noreply.github.com>
2025-06-24 08:00:24 +0000

    inbuilt maths and date

---
+++ restart/inbuilt.js
@@ -0,0 +1,14 @@
+console.log(Math.PI);
+console.log(Math.max(10,20,2200));
+console.log(Math.min(10,500,95));
+console.log(Math.round(1.4));
+console.log(Math.ceil(1.1));
+console.log(Math.floor(1.1));
+console.log(Math.abs(-5)); 
+
+let curr=new Date();
+let date=new Date('july 2 2002 07:15');
+let newDate=new Date(1998,7,20,5);
+console.log(curr);
+console.log(date);
+console.log(newDate)
